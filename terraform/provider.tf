7940aa944d49f9cc8f1d69ad59d98aec57a1ced9
Thomas Liu <tyangliu@gmail.com>
2016-04-15 06:03:31 -0700

    Provision minions and launch all daemons

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,6 +15,16 @@ provider "digitalocean" {
 }
 
 ##########################################################################
+# etcd start script
+##########################################################################
+resource "template_file" "start_etcd" {
+	template = "${file("etcd.yaml")}"
+	vars {
+		ETCD_DIR = "/tmp/etcddata"
+	}
+}
+
+##########################################################################
 # etcd node
 ##########################################################################
 resource "digitalocean_droplet" "k8s_etcd" {
@@ -22,6 +32,7 @@ resource "digitalocean_droplet" "k8s_etcd" {
 	name = "k8s-etcd"
 	region = "nyc3"
 	size = "512mb"
+	user_data = "${template_file.start_etcd.rendered}"
 	ssh_keys = [
 		"${var.ssh_fingerprint}"
 	]
@@ -39,13 +50,23 @@ resource "digitalocean_droplet" "k8s_etcd" {
 		destination = "/usr/local/bin/etcd"
 	}
 
-	# Start the etcd daemon
-	provisioner "remote-exec" {
-		inline = [
-			"chmod +x /usr/local/bin/etcd",
-			"mkdir /tmp/etcddata",
-			"etcd -data-dir /tmp/etcddata	--bind-addr=${self.ipv4_address}:4001 >/dev/null 2>/dev/null &"
-		]
+	# Run the start script to start daemons
+	provisioner "local-exec" {
+		command = <<EOF
+			ssh -o "StrictHostKeyChecking no" root@${self.ipv4_address} /home/start.sh
+EOF
+	}
+}
+
+##########################################################################
+# k8s master start script
+##########################################################################
+resource "template_file" "start_master" {
+	template = "${file("master.yaml")}"
+	vars {
+		ETCD_IP = "${digitalocean_droplet.k8s_etcd.ipv4_address}"
+		CLUSTER_CIDR = "10.1.0.0/16"
+		SERVICE_IP_RANGE = "10.0.0.0/24"
 	}
 }
 
@@ -57,6 +78,7 @@ resource "digitalocean_droplet" "k8s_master" {
 	name = "k8s-master"
 	region = "nyc3"
 	size = "512mb"
+	user_data = "${template_file.start_master.rendered}"
 	ssh_keys = [
 		"${var.ssh_fingerprint}"
 	]
@@ -100,15 +122,96 @@ resource "digitalocean_droplet" "k8s_master" {
 		destination = "/usr/local/bin/kube-scheduler"
 	}
 
-	# Start the daemons
-	provisioner "remote-exec" {
-		inline = [
-			"chmod +x -R /usr/local/bin",
-			"docker daemon >/dev/null 2>/dev/null &",
-			"kube-apiserver --allow-privileged --v=5 --insecure-bind-address=${self.ipv4_address} --insecure-port=8080 --etcd-servers=http://${digitalocean_droplet.k8s_etcd.ipv4_address}:4001 --service-cluster-ip-range=10.0.0.0/24 --cors-allowed-origins=. >/dev/null 2>/dev/null &",
-			"sleep 5",
-			"kube-controller-manager --v=5 --enable-hostpath-provisioner=false --allocate-node-cidrs=true --cluster-cidr=10.1.0.0/16 --master=${self.ipv4_address}:8080 >/dev/null 2>/dev/null &",
-			"kube-scheduler --v=5 --master=${self.ipv4_address}:8080 >/dev/null 2>/dev/null &"
-		]
+	# Run the start script to start daemons
+	provisioner "local-exec" {
+		command = <<EOF
+			ssh -o "StrictHostKeyChecking no" root@${self.ipv4_address} /home/start.sh
+EOF
 	}
+}
+
+##########################################################################
+# k8s master start script
+##########################################################################
+resource "template_file" "start_minion" {
+	template = "${file("minion.yaml")}"
+	vars {
+		ETCD_IP = "${digitalocean_droplet.k8s_etcd.ipv4_address}"
+		MASTER_IP = "${digitalocean_droplet.k8s_master.ipv4_address}"
+		MASTER_PORT = 8080
+	}
+}
+
+##########################################################################
+# k8s minion nodes
+##########################################################################
+resource "digitalocean_droplet" "k8s_minion" {
+	count = "${var.num_workers}"
+
+	image = "ubuntu-15-10-x64"
+	name = "${format("k8s-minion-%02d", count.index + 1)}"
+	region = "nyc3"
+	size = "512mb"
+	user_data = "${template_file.start_minion.rendered}"
+	ssh_keys = [
+		"${var.ssh_fingerprint}"
+	]
+
+	connection {
+		user = "root"
+		type = "ssh"
+		key_file = "${var.pvt_key}"
+		timeout = "2m"
+	}
+
+	# Load dependencies onto machine
+	provisioner "file" {
+		source = "./bin/docker"
+		destination = "/usr/local/bin/docker"
+	}
+	provisioner "file" {
+		source = "./bin/criu"
+		destination = "/usr/local/bin/criu"
+	}
+	provisioner "file" {
+		source = "./bin/protoc"
+		destination = "/usr/local/bin/protoc"
+	}
+	provisioner "file" {
+		source = "./lib/libprotobuf-c.so"
+		destination = "/usr/local/lib/libprotobuf-c.so"
+	}
+
+	# Load k8s binaries onto machine
+	provisioner "file" {
+		source = "./bin/k8s/kubelet"
+		destination = "/usr/local/bin/kubelet"
+	}
+	provisioner "file" {
+		source = "./bin/k8s/kube-proxy"
+		destination = "/usr/local/bin/kube-proxy"
+	}
+
+	# Run the start script to start daemons
+	provisioner "local-exec" {
+		command = <<EOF
+			ssh -o "StrictHostKeyChecking no" root@${self.ipv4_address} /home/start.sh
+EOF
+	}
+}
+
+##########################################################################
+# Setup kubectl
+##########################################################################
+resource "null_resource" "setup_kubectl" {
+	depends_on = ["digitalocean_droplet.k8s_minion"]
+	provisioner "local-exec" {
+		command = <<EOF
+			kubectl config set-cluster local \
+				--server=http://${digitalocean_droplet.k8s_master.ipv4_address}:8080 \
+				--insecure-skip-tls-verify=true
+			kubectl config set-context local --cluster=local
+			kubectl config use-context local
+EOF
+    }
 }
